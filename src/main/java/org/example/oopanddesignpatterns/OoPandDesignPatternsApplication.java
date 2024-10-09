07069351c759017db77ff272b2a73189ce7ba5f6
Evgeniy Sidun <zsydun@gamil.com>
2024-10-09 22:08:02 +0300

    first commit

---
+++ src/main/java/org/example/oopanddesignpatterns/OoPandDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package org.example.oopanddesignpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoPandDesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OoPandDesignPatternsApplication.class, args);
+    }
+
+}
